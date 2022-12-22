164397e851b8ad17b7f3ed6d3a6fb3cd8d376892
KarimovProject <abdullatifkarimov772@gmail.com>
2022-12-22 22:31:21 +0500

    list of dict

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from random_user import RandomUser
+import pprint
 
 # Create a new instance of the RandomUser class
 user = RandomUser()
-# Print the first name of the user
-print(user.get_first_name())
-
-# Print the last name of the user
-print(user.get_last_name())
-
-# Print the full name of the user
-print(user.get_full_name())
+get_user_full_about ={
+    'first_name':user.get_first_name(),
+    'last_name':user.get_last_name(),
+    'phone': user.get_phone(),
+    'picture':user.get_picture(),
+    'email':user.get_email(),
+    'location':user.get_location()
+}
+get_users_num = 20
+s= 0
+get_about_user=[]
+while s!=get_users_num:
+    
+    get_about_user.append(get_user_full_about)
+    s+=1
+    
+pprint.pprint(get_about_user)
